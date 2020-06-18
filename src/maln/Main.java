fb171bfad6d7f5db4926f97c2bb1904b06fe65d9
barteks2236 <barteks2236@wp.pl>
2020-06-18 20:41:32 +0200

    dodane 3:>

--- src/maln/Main.java
+++ src/maln/Main.java
@@ -1,4 +1,4 @@
-package main;
+package maln;
 
 public class Main {
 
--- src/model/Skladnik.java
+++ src/model/Skladnik.java
@@ -4,26 +4,27 @@ public class Skladnik {
 
 	private String nazwa;
 
-	private double cena;
+	private char cena;
 
 	private int kodKreskowy;
 
-	public Skladnik(String nazwa, double cena, int kodKreskowy) {
+	public Skladnik(String nazwa, char cena, char kodKreskowy) {
 		this.nazwa = nazwa;
 		this.cena = cena;
 		this.kodKreskowy = kodKreskowy;
 	}
 
-	public String getNazwa() {
-		return nazwa;
-	}
-
-	public double getCena() {
+	public char getNazwa() {
 		return cena;
 	}
 
-	public int getKodKreskowy() {
-		return kodKreskowy;
+	public char getCena() {
+		return cena;
+		
+	}
+
+	public void getKodKreskowy() {
+	
 	}
 
 }
